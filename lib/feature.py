49262351d8d5c932552c438d6b7372fad2e4a9fd
Michael Hein <mh6620@princeton.edu>
2024-12-02 11:36:16 -0500

    added n_chroma argument

--- lib/feature.py
+++ lib/feature.py
@@ -84,7 +84,7 @@ def beat_mfcc(path):
 @memory.cache
 def chroma_cens(path):
   audio_signal, sr = librosa.load(path)
-  chroma_cens_ = librosa.feature.chroma_cens(y=audio_signal, sr=sr)
+  chroma_cens_ = librosa.feature.chroma_cens(y=audio_signal, sr=sr, n_chroma=12)
   return chroma_cens_
 
 
