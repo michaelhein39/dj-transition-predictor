ccf18a2fa655b2f8e4c49fcd9f5558ebf87e5056
Michael Hein <mh6620@princeton.edu>
2025-03-24 17:05:20 -0400

    changed calculate bpm

--- scripts/segmentation.py
+++ scripts/segmentation.py
@@ -13,11 +13,11 @@ from madmom.features.beats import RNNBeatProcessor, DBNBeatTrackingProcessor
 
 Case = namedtuple('Case', ['features', 'key_invariant'])
 CASES = [
-  Case(features=['mfcc'], key_invariant=False),
-  Case(features=['chroma'], key_invariant=False),
-  Case(features=['chroma'], key_invariant=True),
-  Case(features=['chroma', 'mfcc'], key_invariant=False),
-  Case(features=['chroma', 'mfcc'], key_invariant=True),
+    Case(features=['mfcc'], key_invariant=False),
+    Case(features=['chroma'], key_invariant=False),
+    Case(features=['chroma'], key_invariant=True),
+    Case(features=['chroma', 'mfcc'], key_invariant=False),
+    Case(features=['chroma', 'mfcc'], key_invariant=True),
 ]
 
 df_tlist = pd.read_csv('data/meta/tracks_trunc.csv')
@@ -40,96 +40,100 @@ df_trans = prev.merge(next, on='i_track_next', suffixes=('_prev', '_next'))
 
 
 def main():
-  os.makedirs('data/segment', exist_ok=True)
-  data = []
-  for _, mix in df_mlist.iterrows():
-    result = segmentation(mix.mix_id)
-    data.append(result)
-  df = pd.concat(data, ignore_index=True)
-  df.to_pickle('data/segment/all_mix_segmentation.pkl')  # Overwrites existing file
+    os.makedirs('data/segment', exist_ok=True)
+    data = []
+    for _, mix in df_mlist.iterrows():
+        result = segmentation(mix.mix_id)
+        data.append(result)
+    df = pd.concat(data, ignore_index=True)
+    df.to_pickle('data/segment/all_mix_segmentation.pkl')  # Overwrites existing file
 
 
 
 def segmentation(mix_id):
-  result_path = f'data/segment/{mix_id}.csv'
-  if os.path.isfile(result_path):
-    print(f'=> Skip processing: {result_path}')
-    return pd.read_pickle(result_path)
+    result_path = f'data/segment/{mix_id}.csv'
+    if os.path.isfile(result_path):
+        print(f'=> Skip processing: {result_path}')
+        return pd.read_pickle(result_path)
 
-  df_mix_trans = df_trans
-  df_mix_align = df_align
-  df_mix_align_prev = df_mix_align.copy()
-  df_mix_align_prev.columns = df_mix_align_prev.columns + '_prev'
-  df_mix_align_prev = df_mix_align_prev.rename(columns={'case_prev': 'case'})
-  df_mix_align_next = df_mix_align.copy()
-  df_mix_align_next.columns = df_mix_align_next.columns + '_next'
-  df_mix_align_next = df_mix_align_next.rename(columns={'case_next': 'case'})
-  df = df_mix_trans.merge(df_mix_align_prev).merge(df_mix_align_next)
+    df_mix_trans = df_trans
+    df_mix_align = df_align
+    df_mix_align_prev = df_mix_align.copy()
+    df_mix_align_prev.columns = df_mix_align_prev.columns + '_prev'
+    df_mix_align_prev = df_mix_align_prev.rename(columns={'case_prev': 'case'})
+    df_mix_align_next = df_mix_align.copy()
+    df_mix_align_next.columns = df_mix_align_next.columns + '_next'
+    df_mix_align_next = df_mix_align_next.rename(columns={'case_next': 'case'})
+    df = df_mix_trans.merge(df_mix_align_prev).merge(df_mix_align_next)
 
-  # Each row of df represents a transition between two tracks.
-  # It has columns pertaining to the tracklist info of each track, as well as
-  # the alignment info between the two tracks.
+    # Each row of df represents a transition between two tracks.
+    # It has columns pertaining to the tracklist info of each track, as well as
+    # the alignment info between the two tracks.
 
-  mix_path = f'data/mix/{mix_id}.wav'
-  mix_beat_times = beat_times(mix_path)
+    mix_path = f'data/mix/{mix_id}.wav'
+    mix_beat_times = beat_times(mix_path)
 
-  results = []
-  for idx, row in df.iterrows():
-    if (row['wp_prev'][-1, 0] != 0) or (row['wp_next'][-1, 0] != 0):
-      # Some weird warping path results...
-      # I guess the tracks are too long so they are considered as the longer sequence?
-      print(f'=> ERROR 1: {mix_id}')
-      continue
+    results = []
+    for idx, row in df.iterrows():
+        print(f'=> {idx}: {mix_id}')
+        if (row['wp_prev'][-1, 0] != 0) or (row['wp_next'][-1, 0] != 0):
+            # Some weird warping path results...
+            # I guess the tracks are too long so they are considered as the longer sequence?
+            print(f'=> ERROR 1: {mix_id}')
+            continue
 
-    if (row['wp_prev'].max() > len(mix_beat_times)) or (row['wp_next'].max() > len(mix_beat_times)):
-      # Beats in the warp path should not be greater than the number of beats in the mix
-      print(f'=> ERROR 2: {mix_id}')
-      continue
+        if (row['wp_prev'].max() > len(mix_beat_times)) or (row['wp_next'].max() > len(mix_beat_times)):
+            # Beats in the warp path should not be greater than the number of beats in the mix
+            print(f'=> ERROR 2: {mix_id}')
+            continue
 
-    path_S1 = f"data/track/{row['filename_prev']}.wav"
-    path_S2 = f"data/track/{row['filename_next']}.wav"
+        path_S1 = f"data/track/{row['filename_prev']}.wav"
+        path_S2 = f"data/track/{row['filename_next']}.wav"
 
-    cue_out_beat_mix = row['mix_cue_out_beat_prev']  # Beginning of transition region
-    cue_in_beat_mix = row['mix_cue_in_beat_next']  # End of transition region
+        cue_out_beat_mix = row['mix_cue_out_beat_prev']  # Beginning of transition region
+        cue_in_beat_mix = row['mix_cue_in_beat_next']  # End of transition region
 
-    cue_out_time_mix = mix_beat_times[cue_out_beat_mix]  # Beginning of transition region
-    cue_in_time_mix = mix_beat_times[cue_in_beat_mix]  # End of transition region
+        cue_out_time_mix = mix_beat_times[cue_out_beat_mix]  # Beginning of transition region
+        cue_in_time_mix = mix_beat_times[cue_in_beat_mix]  # End of transition region
 
-    bpm_orig_S1 = calculate_bpm(path_S1)
-    bpm_orig_S2 = calculate_bpm(path_S2)
-    bpm_target = calculate_bpm(mix_path,
-                               start_time=cue_out_time_mix,
-                               end_time=cue_in_time_mix)
+        print(1)
+        bpm_orig_S1 = calculate_bpm(path_S1)
+        bpm_orig_S2 = calculate_bpm(path_S2)
+        bpm_target = calculate_bpm(mix_path,
+                                   start_time=cue_out_time_mix,
+                                   end_time=cue_in_time_mix)
 
-    result = {
-              'mix_id': mix_id,
-              'case': row['case'],
-              'i_track_S1': row['i_track_prev'],
-              'i_track_S2': row['i_track_next'],
-              'track_id_S1': row['track_id_prev'],
-              'track_id_S2': row['track_id_next'],
-              'cue_out_time_S1': row['track_cue_out_time_prev'],
-              'cue_in_time_S2': row['track_cue_in_time_next'],
-              'path_S1': path_S1,
-              'path_S2': path_S2,
-              'mix_path': mix_path,
-              'cue_out_time_mix': cue_out_time_mix,
-              'cue_in_time_mix': cue_in_time_mix,
-              'bpm_orig_S1': bpm_orig_S1,
-              'bpm_orig_S2': bpm_orig_S2,
-              'bpm_target': bpm_target
-              }
-    results.append(result)
+        print(3)
 
-  # Convert results to DataFrame
-  df_results = pd.DataFrame(results)
+        result = {
+                'mix_id': mix_id,
+                'case': row['case'],
+                'i_track_S1': row['i_track_prev'],
+                'i_track_S2': row['i_track_next'],
+                'track_id_S1': row['track_id_prev'],
+                'track_id_S2': row['track_id_next'],
+                'cue_out_time_S1': row['track_cue_out_time_prev'],
+                'cue_in_time_S2': row['track_cue_in_time_next'],
+                'path_S1': path_S1,
+                'path_S2': path_S2,
+                'mix_path': mix_path,
+                'cue_out_time_mix': cue_out_time_mix,
+                'cue_in_time_mix': cue_in_time_mix,
+                'bpm_orig_S1': bpm_orig_S1,
+                'bpm_orig_S2': bpm_orig_S2,
+                'bpm_target': bpm_target
+                }
+        results.append(result)
 
-  # Each row of df_results is a transition between two tracks.
-  # The columns include the cue in and cue out for each track as calculated in alignment.py
+    # Convert results to DataFrame
+    df_results = pd.DataFrame(results)
 
-  df_results.to_csv(result_path)
-  print(f'=> Saved: {result_path}')
-  return df_results
+    # Each row of df_results is a transition between two tracks.
+    # The columns include the cue in and cue out for each track as calculated in alignment.py
+
+    df_results.to_csv(result_path)
+    print(f'=> Saved: {result_path}')
+    return df_results
 
 
 def calculate_bpm(audio_path, sr=SAMPLING_RATE):
@@ -139,25 +143,18 @@ def calculate_bpm(audio_path, sr=SAMPLING_RATE):
 
 
 def calculate_bpm(audio_path, start_time=None, end_time=None, sr=SAMPLING_RATE):
-    beat_processor = RNNBeatProcessor()
-    beattracking_processor = DBNBeatTrackingProcessor(fps=FPS)
+    print(2)
+    beat_times_ = beat_times(audio_path, start_time=start_time, end_time=end_time, sr=sr)
+    print(2.5)
     
-    # Load the audio and extract the segment
-    if start_time is not None and end_time is not None:
-      y, _ = librosa.load(audio_path, sr=sr, offset=start_time, duration=(end_time - start_time))
-    else:
-      y, _ = librosa.load(audio_path, sr=sr)
-    
-    beat_activations = beat_processor(y)
-    beat_times = beattracking_processor(beat_activations)
-    
-    if len(beat_times) > 1:
-        intervals = np.diff(beat_times)
+    if len(beat_times_) > 1:
+        intervals = np.diff(beat_times_)
         bpm = 60.0 / np.mean(intervals)
     else:
         print(f'=> ERROR: bad segment')
+        bpm = None
     return bpm
 
 
 if __name__ == '__main__':
-  main()
+    main()
