197a3df1e69c943f7a3817c6305ac430c02ac049
Michael Hein <mh6620@princeton.edu>
2025-04-01 19:51:12 -0400

    added new features, fixed indenting

--- scripts/feature_extraction.py
+++ scripts/feature_extraction.py
@@ -8,21 +8,25 @@ from lib.feature import *
 
 
 def main():
-  with Pool(os.cpu_count() // 2) as pool:
-    # paths = [f'data/mix/{mix_id}.wav' for mix_id in df_tlist.mix_id.unique()]
-    # paths += [f'data/track/{filename}.wav'  for filename in df_tlist.filename]
-    mix_paths = glob.glob('data/mix/*.wav')
-    track_paths = glob.glob('data/track/*.wav')
-    paths = mix_paths + track_paths
-    iterator = pool.imap(extract_feature, paths)
-    for _ in tqdm(iterator, total=len(paths)):
-      pass
+    with Pool(os.cpu_count() // 2) as pool:
+        # paths = [f'data/mix/{mix_id}.wav' for mix_id in df_tlist.mix_id.unique()]
+        # paths += [f'data/track/{filename}.wav'  for filename in df_tlist.filename]
+        mix_paths = glob.glob('data/mix/*.wav')
+        track_paths = glob.glob('data/track/*.wav')
+        paths = mix_paths + track_paths
+        iterator = pool.imap(extract_feature, paths)
+        for _ in tqdm(iterator, total=len(paths)):
+            pass
 
 
 def extract_feature(path):
-  beat_chroma_cens(path)
-  beat_mfcc(path)
+    beat_chroma_cens(path)
+    beat_mfcc(path)
+    beat_chroma_cqt(path)
+    beat_spectral_contrast(path)
+    beat_onset_strength(path)
+    beat_downbeat_probabilities(path)
 
 
 if __name__ == '__main__':
-  main()
+    main()
